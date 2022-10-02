24eed4a2e04a3047d6ea376ad1d02594e425d759
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2022-10-02 11:40:02 +0200

    add new exercise

---
+++ algorithmen/test/X.java
@@ -0,0 +1,14 @@
+public class X {
+    static int x;
+
+    public static void main(String[] args) {
+        x = 0;
+        System.out.println(!m() | m() || m() | !m());
+        System.out.println(x);
+    }
+
+    private static boolean m() {
+        x++;
+        return false;
+    }
+}
---
+++ binaryint/src/main/java/converters/base/Updater.java
@@ -0,0 +1,15 @@
+package converters.base;
+
+import javax.swing.text.Document;
+
+/*
+ * Created on 05.09.2014
+ */
+/**
+ * @author Wolfgang Weck
+ */
+public interface Updater<E> {
+	Document getDocument();
+
+	void update(E value);
+}
