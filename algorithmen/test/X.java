c62353ac9b4f041814092c2328b7f4c7522ae618
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2022-10-04 01:10:05 +0200

    add method find largestNumber

--- algorithmen/test/X.java
+++ algorithmen/test/X.java
@@ -5,10 +5,28 @@ public class X {
         x = 0;
         System.out.println(!m() | m() || m() | !m());
         System.out.println(x);
+
+        int[] test = { 0, 5, 5, -2, 100 };
+        System.out.println("x= " + findLargest(test));
+
     }
 
     private static boolean m() {
         x++;
         return false;
+    }
+
+    static int findLargest(int[] numbers) {
+        int largest = numbers[0];
+        if (numbers.length == 1 && numbers[0] == Integer.MIN_VALUE){
+            largest = numbers [0];}
+        else {
+            for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > largest) {
+                largest = numbers[i];
+               }
+            }
+        }
+        return largest;
     }
 }
---
+++ algorithmen/test/largestNumber.java
@@ -0,0 +1,12 @@
+public class largestNumber {
+
+
+    public static void main(String[] args) {
+        //largestNumber();
+
+      
+    }
+
+        
+
+}
