dc63a8d829fbc8eed5fe3b562abbc9ce85c5ecb5
heshamouda <hesham.ouda@outlook.com>
2022-10-04 18:03:15 +0200

    Method find closest positive number

--- algorithmen/test/X.java
+++ algorithmen/test/X.java
@@ -6,8 +6,9 @@ public class X {
         System.out.println(!m() | m() || m() | !m());
         System.out.println(x);
 
-        int[] test = { 0, 5, 5, -2, 100 };
+        int[] test = { 3, 2, 5, 5, -2, 100 };
         System.out.println("x= " + findLargest(test));
+        System.out.println("x= " + closestToZero(test));
 
     }
 
@@ -16,15 +17,48 @@ public class X {
         return false;
     }
 
+    static int closestToZero(int[] ints) {
+        /*
+         * int current = 0;
+         * int near = ints[0];
+         * 
+         * if (ints == null || ints.length == 0) {
+         * return 0;
+         * } else {
+         * 
+         * for (int i = 0; i < ints.length; i++) {
+         * current = Math.abs(ints[i]);
+         * if (current <= near) {
+         * near = ints[i];
+         * }
+         * }
+         * return near;
+         * }
+         */
+        int closestIndex = 0;
+        int diff = Integer.MAX_VALUE;
+        for (int i = 0; i < ints.length; ++i) {
+            int abs = Math.abs(ints[i]);
+            if (abs < diff) {
+                closestIndex = i;
+                diff = abs;
+            } else if (abs == diff && ints[i] > 0 && ints[closestIndex] < 0) {
+                // same distance to zero but positive
+                closestIndex = i;
+            }
+        }
+        return ints[closestIndex];
+    }
+
     static int findLargest(int[] numbers) {
         int largest = numbers[0];
-        if (numbers.length == 1 && numbers[0] == Integer.MIN_VALUE){
-            largest = numbers [0];}
-        else {
+        if (numbers.length == 1 && numbers[0] == Integer.MIN_VALUE) {
+            largest = numbers[0];
+        } else {
             for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] > largest) {
-                largest = numbers[i];
-               }
+                if (numbers[i] > largest) {
+                    largest = numbers[i];
+                }
             }
         }
         return largest;
