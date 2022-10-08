f3925024672bf8803ac5740e75a83d889af91126
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2022-10-08 22:39:43 +0200

    sumit new methods

--- algorithmen/test/X.java
+++ algorithmen/test/X.java
@@ -63,4 +63,5 @@ public class X {
         }
         return largest;
     }
+
 }
--- algorithmen/test/largestNumber.java
+++ algorithmen/test/largestNumber.java
@@ -1,12 +1,23 @@
 public class largestNumber {
 
-
     public static void main(String[] args) {
-        //largestNumber();
+        int[] test = { 3, 2, 5, 5, -2, 100 };
+        System.out.println("x= " + findLargest(test));
 
-      
     }
 
-        
-
+    // largestNumber();
+    static int findLargest(int[] numbers) {
+        int largest = numbers[0];
+        if (numbers.length == 1 && numbers[0] == Integer.MIN_VALUE) {
+            largest = numbers[0];
+        } else {
+            for (int i = 0; i < numbers.length; i++) {
+                if (numbers[i] > largest) {
+                    largest = numbers[i];
+                }
+            }
+        }
+        return largest;
+    }
 }
