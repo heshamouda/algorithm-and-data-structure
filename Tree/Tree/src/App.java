58a55288239447163d4c1ba6a8b9c1f428978979
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2022-10-20 22:52:11 +0200

    call insert method in App

--- Tree/Tree/src/App.java
+++ Tree/Tree/src/App.java
@@ -8,6 +8,15 @@ public class App {
          * -
          * -
          */
+        Tree intTree = new Tree();
+        intTree.insert(25);
+        intTree.insert(30);
+        intTree.insert(15);
+        intTree.insert(10);
+        intTree.insert(27);
+        intTree.insert(29);
+        intTree.insert(26);
+        intTree.insert(40);
 
     }
 }
