464e82b5366a5897a00dfc4824d8a2ab74736cd0
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2022-10-20 22:46:53 +0200

    create  method to insert root

--- Tree/Tree/src/Tree.java
+++ Tree/Tree/src/Tree.java
@@ -2,4 +2,12 @@ public class Tree {
 
     private TreeNode root;
 
+    public void insert(int vlaue) {
+        if (root == null) {
+            root = new TreeNode(vlaue);
+        } else {
+            root.insert(vlaue);
+        }
+    }
+
 }
