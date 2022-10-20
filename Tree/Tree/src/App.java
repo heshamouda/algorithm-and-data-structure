4eb11eba072cdd88b79c9b942f52f20de595fc2d
heshamouda <hesham.ouda@outlook.com>
2022-10-20 15:31:09 +0200

    create new project for tree structure

--- Tree/Tree/src/App.java
+++ Tree/Tree/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // root --> edge --> leaf
+        /*
+         * binary search tree
+         * - every node has 0,1,2 children
+         * - It has left child or right child
+         * -
+         * -
+         */
+
     }
 }
